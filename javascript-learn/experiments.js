d0f830ba135e0786601262cc167e7a017118ab36
An Bast <sebastianrondonja@hotmail.com>
2022-09-27 07:18:29 -0500

    Javascript
    Se estructura el codigo y se inicia el proceso de aprendizaje de javascrit

---
+++ javascript-learn/experiments.js
@@ -0,0 +1,5 @@
+/**
+ * Imprimir en consola
+ * Para abrir la consola hay que abrir la pagina html vinculada al archivo js en el navegador, y en el apartado inspecionar elemento abir la opcion consola
+ */
+console.log("HOLA MUNDO");
